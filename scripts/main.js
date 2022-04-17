62da5ab6384f9289fb0f3a8f584055b3f8e8d4d1
BuildTools <saturna19.stream@outlook.fr>
2022-04-17 13:00:17 +0200

    Saturna2+9

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 let myBtn = document.getElementById("Button1")
 let heading = document.querySelector('h1')
+let paragraphe = document.querySelector('p')
+let clicks = 0
 function setUN() {
     let myusername = prompt('Nom?')
     localStorage.setItem('nom', myusername);
@@ -8,4 +10,6 @@ function setUN() {
 
 myBtn.addEventListener('click', function(){
 setUN();
+clicks = clicks + 1
+paragraphe.textContent = 'Nb de clics = ' + clicks;
 })
